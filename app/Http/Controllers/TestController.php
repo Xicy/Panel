4c1d81c59a92402043819255796dba8fe4c29af9
Umut Akkaya <umutakkaya1996@gmail.com>
2019-02-27 02:49:32 +0300

    --- Orbitali Status --- *

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,15 +3,27 @@
 namespace App\Http\Controllers;
 
 use Orbitali\Foundations\Orbitali;
+use Orbitali\Http\Traits\FormSubmission;
 
 class TestController extends Controller
 {
+    use FormSubmission;
+
     public function nodeDetails(Orbitali $orb)
     {
-        $orb->relation->icon = ["icon-a", "icon-b", "icon-c"];
-        $orb->relation->title = ["title-a", "title-b", "title-c"];
-        $orb->relation->detail = ["detail-a", "detail-b", "detail-c"];
-        groupExpander($orb->relation, ['icon', 'title', 'detail']);
+        /* Orbitali::macro("groupExpander",function ($name,$relation,$keys){
+            $this->$name = groupExpander($relation,$keys);
+         });
+
+         $orb->groupExpander('galary',$orb->relation, ['icon', 'title', 'detail']);
+         dd($orb);*/
+
+        return response("
+       <form action='#' method='post'>
+       <input type='hidden' name='form_key' value='test_form'/>
+       <input type='text' name='test'/>
+        </form>
+       ");
     }
 
 
